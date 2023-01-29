5bcf90adb312ed2af09837b7db47cbe9b89451c8
varunmkv7 <113337312+varunmkv7@users.noreply.github.com>
2023-01-29 22:06:01 +0530

    output

---
+++ Medical Diagnosis37.pl
@@ -0,0 +1,21 @@
+symptom('Flu').
+symptom('Yellowish eyes and skin').
+symptom('Dark color urine').
+symptom('Pale bowel movement').
+symptom('Fatigue').
+symptom('Vomitting').
+symptom('Fever').
+symptom('Pain in joints').
+symptom('Weakness').
+symptom('Stomach Pain').
+
+treatment('Flu', 'Drink hot water, avoid cold eatables.').
+treatment('Yellowish eyes and skin', 'Put eye drops, have healthy sleep, do not strain your eyes.').
+treatment('Dark color urine', 'Drink lots of water, juices and eat fruits. Avoid alcohol consumption.').
+treatment('Pale bowel movement', 'Drink lots of water and exercise regularly.').
+treatment('Fatigue', 'Drink lots of water, juices and eat fruits.').
+treatment('Vomitting', 'Drink salt and water.').
+treatment('Fever', 'Put hot water cloth on head and take crocin.').
+treatment('Pain in Joints', 'Apply pain killer and take crocin.').
+treatment('Weakness', 'Drink salt and water, eat fruits.').
+treatment('Stomach Pain', 'Avoid outside food and eat fruits.').
